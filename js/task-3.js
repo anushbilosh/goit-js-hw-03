635470756b1e3ab60d63fdf0d153a8ff9065d583
Anush Biloshytska <biloshytska.anush@icloud.com>
2025-03-22 18:54:38 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+function checkForSpam(message) {
+    const banWord1 = message.toLowerCase().includes("spam");
+    const banWord2 = message.toLowerCase().includes("sale");
+    return banWord1 || banWord2 
+}
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
